6f0e5bd5e77b08ef82c2b7e66205e51abbb39b1a
toranger <18004535090@163.com>
2016-12-09 21:47:15 +0800

    the protocal and the dataset
    version 1.0

---
+++ svn_vs/ServerManager/DataSet/DataSet.cpp
@@ -0,0 +1,13 @@
+#include "DataSet.h"
+
+//add 
+bool CDataSet :: AddUser(unsigned long lUserID, unsigned short wPasswordLen,
+const char* pPassword){
+
+}
+bool CDataSet :: AddGroup(const char* pGroupName){
+
+}
+bool CDataSet :: AddProject(const char* pProName){
+
+}
---
+++ svn_vs/ServerManager/DataSet/Group.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#ifndef __GROUP_H__
+#define __GROUP_H__
+#include <Windows.h>
+#include <list>
+#include "Common.h"//also use in linux
+
+using namespace std;
+class CGroup
+{
+public:
+	CGroup() : m_lGroupID(0), m_wGroupNameLen(0)
+	{
+	}
+	
+	void SetGroupName(unsigned short wLen,char* name)
+	{
+		m_wGroupNameLen = wLen;
+		memcpy(m_szGroupName,name,wLen);
+	}
+	bool AddUser(unsigned long lUserID)
+	{
+		list<unsigned long>::iterator it = m_oUserList.begin();
+		while(it != m_oUserList.end()){
+			if(*it == lUserID){//already have this user id
+				return false;
+			}
+			it++;
+		}
+		m_oUserList.push_back(lUserID);
+		return true;
+	}
+	bool DelUser(unsigned long lUserID)
+	{
+		list<unsigned long>::iterator it = m_oUserList.begin();
+		while(it != m_oUserList.end()){
+			if(*it == lUserID){
+				m_oUserList.erase(it);//find
+				return true;
+			}
+			it++;
+		}
+		return false;
+	}
+private:
+	unsigned long m_lGroupID;
+	list<unsigned long> m_oUserList;//the user id in group list
+	unsigned short m_wGroupNameLen;
+	char m_szGroupName[DEF_GROUP_NAME_LEN];
+};
+
+
+
+
+
+
+
+
+
+
+#endif//__GROUP_H__
